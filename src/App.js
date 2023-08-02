a34258b661afd7f5efbc4b06627abbda62508b3f
Ganesh.Maganti <Ganesh.Maganti@eclerx.com>
2023-08-02 17:48:57 +0530

    rock paper scissor

--- src/App.js
+++ src/App.js
@@ -1,94 +1,120 @@
-import Game from './components/Game';
 import { useState } from 'react';
+import './App.css'
 
 function App() {
-  const[gameItems, setGameItems] = useState(["rock", "paper", "scissor"])
-  const[selectedItem, setSelectedItem] = useState("")
-  const[randomItem,setRandomItem] = useState("")
+  const[gameItems, setGameItems] = useState([
+    {id: 1, name: "rock", imageUrl: "https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/rock.png"}, 
+    {id: 2, name: "paper", imageUrl: "https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/paper.png"}, 
+    {id: 3, name: "scissor", imageUrl: "https://rock-paper-scissors-lilac-alpha.vercel.app/static/media/scissors.png"}
+  ])
   const[isShow, setIsShow] = useState(false)
   const[output, setOuptput] = useState("")
   const [gameScore, setGameScore] = useState(0)
+  const [isWin, setIsWin] = useState(false)
+  const [selectedItem, setSelectedItem] = useState([])
+  const [randomItem, setRandomItem] = useState([])
 
-  const clickItem = (data) =>{
-    const random = gameItems[Math.floor(Math.random() * gameItems.length)]
+  const clickItem = (item) =>{
+    const randomOne = gameItems[Math.floor(Math.random() * gameItems.length)].name
 
     let result = ""
     let score = 0
 
-    if(random === "rock" && data === "rock"){
-      result = "draw"
+    setSelectedItem(gameItems.filter((ele) => ele.name === item))
+    setRandomItem(gameItems.filter((ele) => ele.name === randomOne))
+
+    if(randomOne === "rock" && item === "rock"){
+      result = "It's Draw"
       score = gameScore
     }
-    if(random === "rock" && data === "paper"){
+    if(randomOne === "rock" && item === "paper"){
       result = "You Win"
       score = gameScore + 1
     }
-    if(random === "rock" && data === "scissor"){
+    if(randomOne === "rock" && item === "scissor"){
       result = "You Lose"
-      score = 0
-    }
-    if(random === "paper" && data === "paper"){
-      result = "draw"
       score = gameScore
     }
-    if(random === "paper" && data === "rock"){
-      result = "You Lose"
-      score = 0
+    if(randomOne === "paper" && item === "paper"){
+      result = "It's Draw"
+      score = gameScore
     }
-    if(random === "paper" && data === "scissor"){
+    if(randomOne === "paper" && item === "rock"){
+      result = "You Lose"
+      score = gameScore
+    }
+    if(randomOne === "paper" && item === "scissor"){
       result = "You Win"
       score = gameScore + 1
     }
-    if(random === "scissor" && data === "scissor"){
-      result = "draw"
+    if(randomOne === "scissor" && item === "scissor"){
+      result = "It's Draw"
       score = gameScore
     }
-    if(random === "scissor" && data === "rock"){
+    if(randomOne === "scissor" && item === "rock"){
       result = "You Win"
       score = gameScore + 1
     }
-    if(random === "scissor" && data === "paper"){
+    if(randomOne === "scissor" && item === "paper"){
       result = "You Lose"
-      score = 0
+      score = gameScore
     }
 
-    setSelectedItem(data)
+    if(result !== "You Lose"){
+      setIsWin(true)
+    }
+
     setGameScore(score)
-    setRandomItem(random)
     setIsShow(true)
     setOuptput(result)
     
   }
 
   const playAgain = () =>{
-    setSelectedItem("")
-    setRandomItem("")
     setIsShow(false)
     setGameScore(gameScore)
+    setOuptput("")
+    setIsWin(false)
   }
 
   const resetPlay = () =>{
-    setSelectedItem("")
-    setRandomItem("")
     setIsShow(false)
     setGameScore(0)
+    setOuptput("")
   }
 
   return (
-    <div style={{position: "absolute", left: "100px", top: "100px", listStyleType: "none"}}>
-      <p>score: {gameScore}</p>
-      {!isShow && gameItems.map((item) =>(
-        <Game key={item} data={item} clickItem={clickItem}/>
-      ))}
-      {isShow && (
-        <>
-          <p>{selectedItem}</p>
-          <p>{randomItem}</p>
-          {output === "You Lose" && <button onClick={resetPlay}>Reset</button>}
-          {output !== "You Lose" && <button onClick={playAgain}>Play Again</button>}
-        </>
-      )}
-      <p>{output}</p>
+    <div className='app-container'>
+      <div className='top-header'>
+        <h1>Rock Paper Scissor</h1>
+        <span> Your Score: {gameScore}</span>
+      </div>
+      <div className='bottom-container'>
+        <div className='game-container'>
+          <div className='game-card'>
+            {!isShow && gameItems.map(({id,name,imageUrl}) =>(
+                <button key={id} onClick={() => clickItem(name)} className='item-button'><img className='image' src={imageUrl} alt={name}/></button>
+            ))}
+            {isShow && selectedItem.map(({id,name,imageUrl}) =>(
+              <div className='item' key={id}>
+                <button className='item-button'><img className='images' src={imageUrl} alt={name}/></button>
+                <h1 className='person'>You</h1>
+              </div>
+            ))}
+            {isShow && randomItem.map(({id,name,imageUrl}) =>(
+              <div className='item' key={id}>
+                <button className='item-button'><img className='images rev' src={imageUrl} alt={name}/></button>
+                <h1 className='person'>Opponent</h1>
+              </div>
+            ))}
+          </div>
+          <div className='result-container'>
+            <h1 className='result'>{output}</h1>
+            {!isWin && isShow && <button className='result-button' onClick={resetPlay}>Reset</button>}
+            {isWin && isShow && <button className='result-button' onClick={playAgain}>Play Again</button>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
